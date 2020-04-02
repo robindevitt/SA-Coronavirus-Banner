7655a3864a49388aeb573361e4ca7fa222fb15dd
Robin Devitt <robin@successagency.com>
2020-04-02 23:51:53 +0200

    update uninstall fille

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 //define a vairbale and store an option name as the value.
-$option_name = 'rbd_options';
+$option_name = 'banner_options';
 
 //call delete option and use the vairable inside the quotations
 delete_option( $option_name );
